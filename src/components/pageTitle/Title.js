4b041352213c4b70518bafd37cf25904dd5c174d
oni193 <keykeykey193@gmail.com>
2020-12-16 17:33:12 +0900

    ✨ Add common component, image BodyText, Subtitle, Title 입력. 수정 필요. 뭔가 이상함.

---
+++ src/components/pageTitle/Title.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Bar from '../../assets/barBlue.png';
+
+const Title = () => {
+
+    const Bar = {
+
+    }
+
+    return(
+        <div>
+            <img src={Bar} alt="Bar"/>
+            {/* {title} */}
+        </div>
+    );
+};
+
+export default Title;

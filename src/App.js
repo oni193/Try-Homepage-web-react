3af5c937aef548a71166d964f29c4b873b51cc9c
oni193 <keykeykey193@gmail.com>
2020-12-16 17:34:28 +0900

    ➕ Add App's route to Title import title from 블라 를 App.js에 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Title from './components/pageTitle/Title'
 
 function App() {
   return (
     <div>
     <Header/>
+    <Title/>
     <Footer/>
     </div>
   );

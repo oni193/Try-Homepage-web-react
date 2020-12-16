207f3e61a72c30a54f7a6980c0941ed81c247fba
oni193 <keykeykey193@gmail.com>
2020-12-16 14:21:02 +0900

    ✨ Add MobileFooter Components

--- src/components/footer/MobileFooter.js
+++ src/components/footer/MobileFooter.js
@@ -3,15 +3,31 @@ import Logo from '../../assets/logoWhite.png';
 
 const MobileFooter = () => {
 
-    const logoStyle ={
-        width: '100px',
+    const componentStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        width: '100%',
+        height: '100px',
+        background: 'GREEN'
+    }
+
+    const logoStyle = {
+        width: '91px',
         height: 'auto'
-    };
+    }
     
-    return(
-        <div>
-            <img src={Logo} style={logoStyle} alt="logo img" />
+    const company = companyInfo.map((data, i) => (
+        <div key={'info' + i}>
+            {data}
         </div>
+    ));
+  
+    return(
+            <div style={componentStyle}>
+                <img src={Logo} style={logoStyle} alt="logo img" />
+                <div>{company}</div>
+            </div>
     );
 };
 

a661a4b073dac7c07217c8fdb10b4d9601226b9b
oni193 <keykeykey193@gmail.com>
2020-12-17 16:07:16 +0900

    ✨ Add Navigation 다 틀려먹어서 다 주석 처리 해놨음 다시 해야함

---
+++ src/components/Navigation.js
@@ -0,0 +1,47 @@
+import React , { useState }from 'react';
+import logoWhite from '../assets/logoWhite.png';
+import hamburgerWhite from '../assets/hamburgerWhite.png';
+
+
+const Navigation = () => {
+    
+    return(
+        <div>
+            
+        </div>
+    );
+};
+
+// const Navigation = () => {
+//     const navStyle = {
+//         listStyle: 'none',
+//         width: '100%',
+//         height: '100px',
+//         backgroundColor: 'black'
+//     };
+
+//     const logoStyle = {
+//         width: '100px',
+//         height: 'auto'
+//     };
+//     const btnStyle = {
+//         posiion: 'fixed',
+//         width: '30px',
+//         outline: 'none',
+//         transition: '0.4s',
+//     };
+
+//     return(
+//         <div style={navStyle}>
+//             <img src={logoWhite} style={logoStyle}/>
+//             <img src={hamburgerWhite} style={btnStyle} className="hambugButton"/>
+//             <ul style={navStyle}>
+//                 <li>블라</li>
+//                 <li>율러</li>
+//                 <li>울라숑</li>
+//             </ul>
+//         </div>
+//     );
+// };
+
+export default Navigation;

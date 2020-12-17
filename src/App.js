4802d1a679e1daec1b61a76bc4e333eae349c078
oni193 <keykeykey193@gmail.com>
2020-12-17 10:21:07 +0900

    🚚 Move App's import order App.js에서 import 부분 순서대로 위치 변경

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Footer from './components/Footer'
 import Header from './components/Header'
 import Company from './components/Company'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
     <Header/>
     <Company/>
+    <Contact/>
     <Footer/>
     </div>
   );

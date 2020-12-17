d498e1da9cbe0cccd14c084ce332b8804192f5a9
oni193 <keykeykey193@gmail.com>
2020-12-17 10:21:46 +0900

    ✨ Add Contact ( except map.api ) 지도 api 추가하기 전 Contact Components

---
+++ src/components/Contact.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Title from './pageTitle/Title';
+import BodyText from './pageTitle/BodyText';
+// 아이콘
+import marker from '../assets/marker.png';
+import phone from '../assets/phone.png';
+
+const Contact = () => {
+
+    const data ={
+        title: 'CONTACT',
+        body: ['서울 강남구 테헤란로 623 14층',
+                '02-3453-3864']
+    }
+
+    const contactStyle ={
+        borderTop :'3px solid #c0eb75'
+    }
+    
+    return (
+        <div style={contactStyle}>
+        <Title text={data.title}/>
+        <img src={marker}/><BodyText text={data.body[0]}/>
+        <img src={phone}/><BodyText text={data.body[1]}/>
+        </div>
+    );
+}
+export default Contact;

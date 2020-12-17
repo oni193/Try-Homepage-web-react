74b295e758eee5e932c31efcdb15940637883fd1
oni193 <keykeykey193@gmail.com>
2020-12-17 09:40:36 +0900

    ✨ Add Subtitle in Company Company.js 에 subtitle 추가

--- src/components/Company.js
+++ src/components/Company.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Title from './pageTitle/Title';
+import SubTitle from './pageTitle/SubTitle';
 
 const Company = () => {
     
     const data = {
-        title: 'COMPANY'
+        title: 'COMPANY',
+        sub: ['편의성에 집중한','금융 서비스를 만듭니다'],
     };
     return(
         <div>
             <Title text={data.title}/>
+            <SubTitle text={data.sub[0]}/>
+            <SubTitle text={data.sub[1]}/>
         </div>
 
     );
